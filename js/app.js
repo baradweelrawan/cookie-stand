c6bb3992a789e7e919f1eed514e2aebef3456072
baradweelrawan <bardaweel_rawan@yahoo.com>
2021-05-04 16:24:46 +0300

    done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 'use strict';
 let  hours = ['6 am','7 am' , '8am ','9am', '10 am ','11 am','12pm','1pm' ,'2 pm ','3pm' ,'4 pm', '5pm','6pm','7pm','8pm ']
 let salmonCookie = [];
+let div =document.getElementById("sales")
+//let div =document.getElementById("footer")
+let table=document.createElement('table')
+div.appendChild(table)
+let sumOfTotal=0;
+//let totalPerHour= 0
+
 function Cookie (location,seatMin,seatMax,avgCookie)
 {
    this.location = location ;
@@ -10,6 +17,8 @@ function Cookie (location,seatMin,seatMax,avgCookie)
    this.randomCustPerHour=[];
    this.salesPerHour=[];
    this.totalPerHour= 0,
+  // this.grandTotals=[];
+
    salmonCookie.push(this);
 
 }
@@ -22,7 +31,7 @@ Cookie.prototype.getRandom = function (seatMin ,seatMax)
     this.randomCustPerHour.push(Math.floor(Math.random() * (seatMax - seatMin) + seatMin));
   //  return Math.floor(Math.random() * (seatMax - seatMin) + seatMin);
   }
-  console.log(this.randomCustPerHour)
+//  console.log(this.randomCustPerHour)
 }
     
 Cookie.prototype.customerNum = function ()
@@ -31,191 +40,59 @@ Cookie.prototype.customerNum = function ()
   {
    this.salesPerHour.push(Math.ceil(this. randomCustPerHour[j] * this.avgCookie));
    this.totalPerHour += this.salesPerHour[j];
+  // console.log(this.salesPerHour[j])
   }
-  console.log(this.salesPerHour)
+ //console.log(this.salesPerHour)
+ console.log( this.totalPerHour)
 }
-Cookie.prototype.DailyLocationTotal = function ()
-{
-  for(let r=0 ; r<= hours.length ;r++ )
+function tableHead (){
+  let tr=document.createElement('tr')
+  table.appendChild(tr)
+  let td =document.createElement('td')
+  tr.appendChild(td)
+  td.textContent='location Name'
+  for (let i=0 ;i<=hours.length;i++ )
   {
-    this.DailyTotal = totalPerHour[r] +totalPerHour[loca];
-    //this.total = 
+    let td=document.createElement('td')
+    tr.appendChild(td)
+    td.textContent=hours[i]
   }
-  console.log(this.DailyTotal)
-}
-Cookie.prototype.LocationTotal = function ()
-{
-  for (let loca =0 ; loca <= this.location ;loca++)
-  {
-    this.TotalOfLocation = totalPerHour[loca];
-    
-  }
-  console.log(this.TotalOfLocation)
-}
+  let td2 =document.createElement('td')
+  tr.appendChild(td2)
+  td2.textContent='totalOftoal'
 
-
+}
+tableHead()
 
 Cookie.prototype.render = function ()
 {
-  let divEl = document.getElementById('SalmonCookie');
-  let h1El =document.createElement('h1');
-  divEl.appendChild(h1El);
-  h1El.textContent= this.location;
-  let ulEl = document.createElement('ul');
-  divEl.appendChild(ulEl);
-  for (let i =0 ; i <=hours.length ; i++)
-    {
-        let liEl = document.createElement('li');
-        ulEl.appendChild(liEl);
-        liEl.textContent = `${hours[i]}:${this.salesPerHour[i]} cookies`
-   }
- let tableEl = document.createElement('table');
- divEl.appendChild( tableEl); 
- let trEl =document.createElement('tr');
- tableEl.appendChild(trEl);
- for (let hou =0 ; hou <= hours.length ; hou++)
- {
-   let thEl1 =document.createElement('th');
-   thEl1.textContent =hours[hou];
-   trEl.appendChild(thEl1) ;
- }
- let trEl2 =document.createElement('tr');
- tableEl.appendChild( trEl2);
- for (let loc =0 ; loc <=this.location ; loc++)
- {
-  let thEl2 =document.createElement('th');
-  thEl2.textContent =this.location;
-  trEl2.appendChild(thEl2) ;
- }
-/*
- let thEl0 =document.createElement('th');
- for (let i = 0; i < hours.length ; i++)
+  let tableBody = document.createElement('tr');
+  table.appendChild(tableBody);
+  let td =document.createElement('td')
+  tableBody.appendChild(td)
+  td.textContent=this.location
+   
+  for (let j = 0 ; j <= hours.length ;j++)
   {
-   let thEl0 = document.createElement('th')
-   thEl0.textContent = this.totalPerHour;
-   trEl.appendChild(thEl0) ;
+   let td01 = document.createElement('td');
+   tableBody.appendChild( td01);
+   td01.textContent= this.salesPerHour[j];
   }
-  */
-/*
+  let td3 =document.createElement('td')
+  tableBody.appendChild(td3)
+  td3.textContent=this.totalPerHour
 
-    let liEl = document.createElement('li');
-    liEl.textContent = this.likes[i];
-    ulEl.appendChild(liEl)
- let thEl01 =document.createElement('th');
- thEl01.textContent = this.location;
- trEl.appendChild(thEl01) ;
-
- let thEl02 =document.createElement('th');
- thEl02.textContent = this.location;
- trEl.appendChild(thEl02) ;
- */
-/*
- let thEl1 =document.createElement('th');
- thEl1.textContent = hours[0];
- trEl.appendChild(thEl1) ;
-
- let thEl2 =document.createElement('th');
- thEl2.textContent =hours[1];
- trEl.appendChild(thEl2) ;
-
- let thEl3 =document.createElement('th');
- thEl3.textContent =hours[2];
- trEl.appendChild(thEl3) ;
-
- let thEl4 =document.createElement('th');
- thEl4.textContent =hours[3];
- trEl.appendChild(thEl4) ;
-
- let thEl5 =document.createElement('th');
- thEl5.textContent =hours[4];
- trEl.appendChild(thEl5) ;
-
- let thEl6 =document.createElement('th');
- thEl6.textContent =hours[5];
- trEl.appendChild(thEl6) ;
-
- let thEl7 =document.createElement('th');
- thEl7.textContent =hours[6];
- trEl.appendChild(thEl7) ;
-
- let thEl8 =document.createElement('th');
- thEl8.textContent =hours[7];
- trEl.appendChild(thEl8) ;
-
- 
- /*
- let trEl0 =document.createElement('tr');
- tableEl.appendChild(trEl0); 
-*/
- let tableBody = document.createElement('tr');
- tableEl.appendChild(tableBody);
- for (let i = 0 ; i <= hours.length-1 ;i++)
- {
-  let td1 = document.createElement('td');
-  tableBody.appendChild(td1);
-  td1.textContent = this.totalPerHour;
- }
- let tableBody2 = document.createElement('tr');
- tableEl.appendChild(tableBody2 );
- for (let i = 0 ; i <= hours.length-1 ;i++)
- {
-  let td2 = document.createElement('td');
-  tableBody2 .appendChild(td2);
-  td2.textContent = this.totalPerHour;
- }
-
- let tableBody3 = document.createElement('tr');
- tableEl.appendChild(tableBody3 );
- for (let i = 0 ; i <= hours.length-1 ;i++)
- {
-  let td3 = document.createElement('td');
-  tableBody3 .appendChild(td3);
-  td3.textContent = this.totalPerHour;
- }
- let tableBody4 = document.createElement('tr');
- tableEl.appendChild(tableBody4 );
- for (let i = 0 ; i <= hours.length-1 ;i++)
- {
-  let td4 = document.createElement('td');
-  tableBody4 .appendChild(td4);
-  td4.textContent = this.totalPerHour;
- }
-
- let tableBody5 = document.createElement('tr');
- tableEl.appendChild(tableBody5 );
- for (let i = 0 ; i <= hours.length-1 ;i++)
- {
-  let td5 = document.createElement('td');
-  tableBody5 .appendChild(td5 );
-  td5 .textContent = this.totalPerHour;
- }
-
- 
- /*
-
- let td2 = document.createElement('td');
- tableBody.appendChild(td2);
- td2.textContent = this.totalPerHour;
-
- let td3 = document.createElement('td');
- tableBody.appendChild(td3);
- td3.textContent = this.totalPerHour;
-
- let td4 = document.createElement('td');
- tableBody.appendChild(td4);
- td4.textContent = this.totalPerHour;
-
- let td5 = document.createElement('td');
- tableBody.appendChild(td5);
- td5.textContent = this.totalPerHour;
- */
+  console.log (this.salesPerHour)
+   
 }
+
+
 
 let Seattle = new Cookie('Seattle', 23,65,6.3);
 Seattle.getRandom(23 ,65);
 Seattle.customerNum();
 Seattle.render();
-
+//tableHead.render();
 let Tokyo = new Cookie('Tokyo', 3, 24, 1.2);
 Tokyo.getRandom(3 ,24);
 Tokyo.customerNum();
@@ -231,3 +108,44 @@ Lima.getRandom(2 ,16);
 Lima.customerNum();
 Lima.render();
 console.log(salmonCookie)
+console.log(salmonCookie)
+
+function tablefooter (){
+  let trEl =document.createElement('tr');
+  table.appendChild(trEl);
+  let td6 =document.createElement('td')
+  trEl.appendChild(td6)
+  td6.textContent='Total'
+ 
+  for(let i =0 ;i< hours.length ; i++)
+  {
+    let hourlyTotals =0;
+    for (let j=0 ; j<salmonCookie.length ; j++)
+    {
+     
+       hourlyTotals += salmonCookie[j].salesPerHour[i];
+       console.log (salmonCookie[j])
+       //console.log()
+      
+          //  console.loge (hourlyTotals)
+    }
+    let td03=document.createElement('td')
+    trEl.appendChild(td03)
+    td03.textContent= hourlyTotals
+    
+   
+  }
+  for(let i =0 ;i< salmonCookie.length ; i++)
+  {
+
+    sumOfTotal+=salmonCookie[i].totalPerHour;
+
+  }
+  
+  let td04=document.createElement('td')
+  trEl.appendChild(td04)
+  td04.textContent=sumOfTotal
+}
+tablefooter()
+
+
